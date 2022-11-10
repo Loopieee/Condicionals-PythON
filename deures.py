9f4b24d5452a5fc4772aa12bc11f5ddda0dd2ccc
Loopieee <114224494+Loopieee@users.noreply.github.com>
2022-11-10 08:49:09 +0100

    Create deures.py

---
+++ deures.py
@@ -0,0 +1,51 @@
+a = 1.5
+b = 5.5
+
+print (b-a)
+print (a+b)
+print (a**b)
+
+a += 1
+print (a)
+a -= 2
+print (a)
+
+print (a%b)
+print (a/b)
+print (a//b)
+
+#2- Posa el teu nom en una variable string i mostra’l per pantalla.
+
+name = ('pol_resa')
+print (name)
+
+
+#4- Qualificacio academica:
+
+nota = input ('que nota tienes?')
+nota = float (nota)
+if nota < 5:
+    print ('suspendido');
+elif 5 < nota < 6:
+    print ('bien');
+elif 6 < nota < 8:
+    print ('notable');
+elif 8 < nota < 10:
+    print ('excelente');
+
+#3- Major d’edat.
+
+edad = input ('Edad?')
+edad = int (edad)
+if edad < 18:
+    print ('eres menor de edad');
+elif edad > 18:
+    print ('eres mayor de edad');
+
+#5 diagrama de fluxos:
+    
+No se abre el valorant = input ('Me abrán baneado?')
+if abre valorant = actualizo 
+print ('me pongo contento')
+else no se abre valorant = baneado
+print ('lloro')
